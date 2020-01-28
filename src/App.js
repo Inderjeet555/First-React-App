6826bc97e1495e503851fbe5b6c49278bc2ba4b9
Inderjeet555 <meet2indersingh@gmail.com>
2020-01-28 17:32:05 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello I am React App</h1>
+      <p>This is Really Working!!!</p>
+      <Person/>
+    </div>
+  );
+}
+export default App;
